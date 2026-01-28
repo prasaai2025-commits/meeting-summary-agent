ad1ca6c6ac015bf6afee656c936b87070d934d17
laxmi randive <prasaai2025@gmail.com>
2026-01-28 10:08:22 +0530

    Update transcribe.py

--- app/transcribe.py
+++ app/transcribe.py
@@ -2,7 +2,7 @@ from faster_whisper import WhisperModel
 import subprocess
 import os
 
-model = whisper.load_model("base")
+model = WhisperModel("base", device="cpu", compute_type="int8")
 
 def extract_audio(video_path: str) -> str:
     audio_path = video_path.rsplit(".", 1)[0] + ".wav"
@@ -10,19 +10,22 @@ def extract_audio(video_path: str) -> str:
     if os.path.exists(audio_path):
         return audio_path
 
-    subprocess.run([
-        "ffmpeg", "-y",
-        "-i", video_path,
-        "-ar", "16000",
-        "-ac", "1",
-        audio_path
-    ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
+    subprocess.run(
+        ["ffmpeg", "-y", "-i", video_path, "-ar", "16000", "-ac", "1", audio_path],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
     return audio_path
+
 
 def transcribe_audio(path: str) -> str:
     if path.lower().endswith((".mp4", ".mkv", ".avi")):
         path = extract_audio(path)
 
-    result = model.transcribe(path)
-    return result["text"]
+    segments, info = model.transcribe(path)
+    text = ""
+
+    for segment in segments:
+        text += segment.text + " "
+
+    return text.strip()
